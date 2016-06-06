b6cc820ce77da051e1f7f81014af793237de1f84
Wing Ming Chan <chanw@upstate.edu>
2016-06-06 10:03:10 -0400

    Reformatted the code.

--- cascade_ws_ns/class_files.php
+++ cascade_ws_ns/class_files.php
@@ -1,19 +1,16 @@
 <?php
 use cascade_ws_utility as utility;
-/*
-use cascade_ws_constants as constants;
-use cascade_ws_asset     as asset;
-use cascade_ws_exception as exception;
-*/
+
 spl_autoload_register('cascade__autoload');
+
 function cascade__autoload( $classname )
 {
-	$array =
-		utility\StringUtility::getExplodedStringArray( "\\", $classname );
-	$size = count( $array );
-	
-	if( $size > 0 )
-		$classname = $array[ $size - 1 ];
+    $array =
+        utility\StringUtility::getExplodedStringArray( "\\", $classname );
+    $size = count( $array );
+    
+    if( $size > 0 )
+        $classname = $array[ $size - 1 ];
     $root_path              = dirname( __FILE__ ) . '/';
     $asset_class_folder     = "asset_classes/";
     $helping_class_folder   = "property_classes/";
@@ -30,3 +27,4 @@ function cascade__autoload( $classname )
     else if( file_exists( $root_path . $utility_class_folder . $file ) )
         require_once( $root_path . $utility_class_folder . $file );
 }
+?>
